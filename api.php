b0ff451467ade8d162d4206793a55536bc9dda23
yellyfall <yellyfall96@gmail.com>
2026-06-20 20:19:03 +0200

    Update api.php

--- api.php
+++ api.php
@@ -11,7 +11,6 @@ header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') { http_response_code(200); exit; }
-"expires": null
 	
 // ── FICHIER DE BASE DE DONNEES ─────────────────────────────────────────────
 define('DB_FILE', __DIR__ . '/myf_data.json');
